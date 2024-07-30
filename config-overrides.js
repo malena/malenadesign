2c7a8f171bc73f742ede50d1e015dfd24f444fcb
Malena Andrade <malena.andrade@gmail.com>
2024-07-30 14:36:00 -0500

    Clean up

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,14 @@
 const path = require("path");
 
 module.exports = {
-  override(config, env) {
+  override: function (config, env) {
     //do stuff with the webpack config...
     return config;
   },
   paths: function (paths, env) {
     // Changing public to static
-    paths.appPublic = path.resolve(__dirname, "docs");
-    paths.appHtml = path.resolve(__dirname, "docs/index.html");
+    paths.appBuild = path.resolve(__dirname, "./docs");
+    paths.appHtml = path.resolve(__dirname, "./docs/index.html");
     return paths;
   },
 };
