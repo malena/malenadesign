1b0a7a47932dea654ca87ead77ce26462a23db55
Malena Andrade <malena.andrade@gmail.com>
2024-09-09 17:28:04 -0500

    Update layout, content, responsive and cleanupt

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,14 +5,14 @@ function About() {
     <section id="About" className="about main-section">
       <div className="section-content about-content">
         <h1 className="section-title about-title">About</h1>
-        <ul className="section-list">
+        <ul className="section-list about-section-list">
           <li className="section-list-item about-list-item">
             <h2 className="section-subtitle about-subtitle">
               Over 15 years experience as a front-end web developer.
             </h2>
             <p className="about-description">
-              Turning full designs into beautiful, responsive web pages and
-              apps.
+              Turning full designs into beautiful, responsive web pages and apps
+              using various frameworks and tools.
             </p>
           </li>
           <li className="section-list-item about-list-item">
@@ -30,11 +30,21 @@ function About() {
           </li>
           <li className="section-list-item about-list-item">
             <h2 className="section-subtitle about-subtitle">
-              I’m a passionate Creative Lead.
+              Mentoring junior devs and facilitating design workshops.
             </h2>
             <p className="about-description">
-              I have lead workshops, hackathons and designathons. I enjoy
-              sharing what I learn.
+              Mentoring and guiding front-end developers in new projects,
+              leading design meetings, workshops and running fun designathons
+              and hackathons.
+            </p>
+          </li>
+          <li className="section-list-item about-list-item">
+            <h2 className="section-subtitle about-subtitle">
+              Collaborating with clients and estimating project timelines.
+            </h2>
+            <p className="about-description">
+              Doumenting and estimating development and design features to
+              maintain timelines within budget.
             </p>
           </li>
         </ul>
