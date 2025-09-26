f04a7ccd5a821a749b2268aa84246499b12b5845
Malena Andrade <malena.andrade@gmail.com>
2025-09-26 17:22:49 -0500

    Small css updates, update base url, add scrolltop

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth", // Optional: for a smooth scrolling animation
+    });
+  }, [location.pathname]); // Re-run effect when the pathname changes
+
+  return children;
+};
+
+export default ScrollToTop;
