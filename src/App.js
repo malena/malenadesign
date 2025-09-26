060c0dc5050a1b92bedc94d95f0ed4cac62232c8
Malena Andrade <malena.andrade@gmail.com>
2025-09-26 17:22:49 -0500

    Small css updates, update base url, add scrolltop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -15,19 +16,21 @@ function App() {
   return (
     <div className="app-container">
       <HashRouter>
-        <Nav />
-        <div className="content-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cases" element={<Cases />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/mibunker" element={<Mibunker />} />
-            <Route path="/ucmaximus" element={<UCMaximus />} />
-            <Route path="/starmaker" element={<Starmaker />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </div>
+        <ScrollToTop>
+          <Nav />
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cases" element={<Cases />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/mibunker" element={<Mibunker />} />
+              <Route path="/ucmaximus" element={<UCMaximus />} />
+              <Route path="/starmaker" element={<Starmaker />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+        </ScrollToTop>
       </HashRouter>
       <Footer />
     </div>
