b100bd4c35c24500aab9c6b490337f79fd7b35c1
Malena Andrade <malena.andrade@gmail.com>
2025-09-30 23:46:40 -0500

    Clean up mobile menu and errors

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -9,6 +9,7 @@ const ScrollToTop = ({ children }) => {
       top: 0,
       left: 0,
 <<<<<<< HEAD
+<<<<<<< HEAD
       behavior: "smooth",
     });
   }, [location.pathname]);
@@ -17,6 +18,11 @@ const ScrollToTop = ({ children }) => {
     });
   }, [location.pathname]); // Re-run effect when the pathname changes
 >>>>>>> 060c0dc5 (Small css updates, update base url, add scrolltop)
+=======
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+>>>>>>> 134decf0 (Clean up mobile menu and errors)
 
   return children;
 };
