3ddb98585c50e72396d5800e9656457b6219c9a2
Malena Andrade <malena.andrade@gmail.com>
2025-10-11 15:14:21 -0500

    Build files

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -8,21 +8,9 @@ const ScrollToTop = ({ children }) => {
     window.scrollTo({
       top: 0,
       left: 0,
-<<<<<<< HEAD
-<<<<<<< HEAD
       behavior: "smooth",
     });
   }, [location.pathname]);
-=======
-      behavior: "smooth", // Optional: for a smooth scrolling animation
-    });
-  }, [location.pathname]); // Re-run effect when the pathname changes
->>>>>>> 060c0dc5 (Small css updates, update base url, add scrolltop)
-=======
-      behavior: "smooth",
-    });
-  }, [location.pathname]);
->>>>>>> 134decf0 (Clean up mobile menu and errors)
 
   return children;
 };
